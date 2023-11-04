ce6618c6f2b89d2a4443674adedc9a4b06559552
Fowaz Amran Alfarez <fowaz.213040008@mail.unpas.ac.id>
2023-11-05 02:30:36 +0700

    Commit ke 5
    Video 5 - Model, Collection & Controller

---
+++ 5_Video5/coba-laravel/storage/framework/views/c9cac91f67e303bed8d938c345f98475.php
@@ -0,0 +1,10 @@
+
+
+<?php $__env->startSection('container'); ?>
+    <h2><?php echo e($post["title"]); ?></h2>
+    <h5><?php echo e($post["author"]); ?></h5>
+    <p><?php echo e($post["body"]); ?></p>
+
+    <a href="/posts">Back To Posts</a>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\fowaz\Documents\applications\coba-laravel\resources\views/post.blade.php ENDPATH**/ ?>
