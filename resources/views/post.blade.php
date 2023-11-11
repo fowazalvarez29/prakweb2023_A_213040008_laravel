22d8327b84acb2dde8d4ad733b0af212ef8c782d
Your Name <fowaz.213040008@mail.unpas.ac.id>
2023-11-11 15:24:31 +0700

    Video 12. Redesign UI

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,11 +1,25 @@
 @extends('layouts.main')
 
 @section('container')
-    <h2>{{ $post->title }}</h2>
-    <p>By. <a href="/author/{{ $post->user->username }} " class="text-decoration-none">{{ $post->user->name }} </a> <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+    <div class="container">
+      <div class="row justify-content-center mb-5">
+        <div class="col-md-8">
+           <h1 class="mb-3">{{$post->title}}</h1>
 
-    <h5>{{ $post->author }}</h5>
-    {!! $post->body !!}
+          <p>By. <a href="/authors/{{$post->user->username}}" class="text-decoration-none">{{$post->user->name}}
+          </a> in <a href="/categories/{{$post->category->slug}}" class="text-decoration-none">{{$post->category->name}}</a></p>
 
-    <a href="/posts" class="d-block mt-3">Back To Posts</a>
+          <img src="https://source.unsplash.com/1200x400?{{$post->category->name }}"
+           alt="{{$post->category->name }}" class="img-fluid">
+
+           <article class="my-3 fs-7">
+            {!!$post->body!!}
+           </article>
+
+
+          <a href="/posts" class="d-block mt-3">Back to posts</a>
+            </div>
+        </div>
+    </div>
+
 @endsection
