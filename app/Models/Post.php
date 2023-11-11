ebed9ec6d32b681734e9b29ba2c4eba34ba40ca4
Your Name <fowaz.213040008@mail.unpas.ac.id>
2023-11-11 15:51:35 +0700

    Video 13. Searching & Pagination

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,7 +11,35 @@ class Post extends Model
 
     // protected $fillable = ['title', 'excerpt', 'body'];
     protected $guarded = ['id'];
+
     protected $with = ['category', 'user'];
+
+
+    public function scopeFilter($query, array $filters) {
+        //pencarian
+       // if(isset($filter['search']) ? $filters['search'] : false) {
+       //     return $query->where('title','like','%' .request('search').'%')
+       //                  ->orWhere('body','like','%'.request('search').'%');
+       // }
+
+        $query->when($filters['search'] ?? false,function($query, $search){
+            return $query->where('title','like','%' .$search.'%')
+                            ->orWhere('body','like','%'.$search.'%');
+        });
+
+        $query->when($filters['category']??false, function($query,$category) {
+            return $query->whereHas('category', function($query) use($category){
+                $query->where('slug', $category);
+            });
+        }); 
+
+        $query->when($filters['author']??false, fn($query, $author)=>
+            $query->whereHas('author', fn($query)=>
+                $query->where('username', $author)
+            )
+            );
+    }
+
 
     public function category()
     {
--- routes/web.php
+++ routes/web.php
@@ -24,13 +24,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => 'home'
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" =>'about',
         "name" => "Fowaz Alvarez",
         "email" => "fowaz.213040008@mail.unpas.ac.id",
         "image" => "contoh.png"
@@ -63,6 +65,7 @@ Route::get('/categories/{category:slug}', function(Category $category) {
 Route::get('/authors/{user:username}', function(User $user) {
     return view('posts', [
         'title' => "Pos t by Author : $user->name",
+        'active'=>'posts',
         'posts' => $user->posts->load('category', 'user'),
     ]);
 });
