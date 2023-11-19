13b5e2740780bda6582b5a5644f58d4da13b77e0
Your Name <fowaz.213040008@mail.unpas.ac.id>
2023-11-19 20:03:52 +0700

    Video 16. User Login & Middleware

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -16,13 +18,21 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
+        $validateData =  $request->validate([
             'name' => 'required|max:255',
             'username' => ['required', 'min:3', 'max:255', 'unique:users'],
-            'email' => 'required|email|unique:users',
+            'email' => 'required|email:dns|unique:users',
             'password' => 'required|min:5|max:255'
         ]);
 
-        dd('berhasil!!');
+
+        // $validateData['password'] = bcrypt($validateData['password']);
+        $validateData['password'] = Hash::make($validateData['password']);
+
+        User::create($validateData);
+
+        // $request->session()->flash('success', 'Registration succesfull! Please Login');
+
+        return redirect('/login')->with('success', 'Registration succesfull! Please Login');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,15 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    // protected $fillable = [
+    //     'name',
+    //     'username',
+    //     'email',
+    //     'password',
+    // ];
+
+    // apa aja yang gak boleh diisi secra massal, misal disini id tidak boleh sedangkan sisanya boleh
+    protected $guarded = ['id'];
 
     /**
      * The attributes that should be hidden for serialization.
