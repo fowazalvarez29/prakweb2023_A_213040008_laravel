88d8926542c64756523e62474f29f50c3203ec01
Your Name <fowaz.213040008@mail.unpas.ac.id>
2023-11-25 17:13:01 +0700

    Video 20. Update & Delete Post

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -72,7 +72,10 @@ class DashboardPostController extends Controller
     //halaman untuk menampilkan ubah data 
     public function edit(Post $post)
     {
-        //
+        return view('dashboard.posts.edit', [
+            'post' => $post,
+            'categories' => Category::all()
+        ]);
     }
 
     /**
@@ -81,7 +84,25 @@ class DashboardPostController extends Controller
     // halaman untuk proses ubah datanya
     public function update(Request $request, Post $post)
     {
-        //
+        $rules = [
+            'title' => 'required|max:255',
+            'category_id' => 'required',
+            'body' => 'required'
+        ];
+
+        if ($request->slug != $post->slug) {
+            $rules['slug'] = 'required|unique:posts';
+        }
+
+        $validatedData = $request->validate($rules);
+
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+        Post::where('id', $post->id)
+            ->update($validatedData);
+
+        return redirect('/dashboard/posts')->with('success', 'New post has been updated!');
     }
 
     /**
@@ -90,7 +111,8 @@ class DashboardPostController extends Controller
     // untuk delete postingan
     public function destroy(Post $post)
     {
-        //
+        Post::destroy($post->id);
+        return redirect('/dashboard/posts')->with('success', 'Post has been deleted!');
     }
 
     public function checkSlug(Request $request)
