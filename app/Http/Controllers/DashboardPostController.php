914f058a5602e1f69a377fa57c23c00566cfdfd6
Your Name <fowaz.213040008@mail.unpas.ac.id>
2023-11-25 18:38:38 +0700

    Video 22. Preview, Update & Delete Image

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class DashboardPostController extends Controller
 {
@@ -94,6 +95,7 @@ class DashboardPostController extends Controller
         $rules = [
             'title' => 'required|max:255',
             'category_id' => 'required',
+            'image' => 'image|file|max:6000',
             'body' => 'required'
         ];
 
@@ -102,6 +104,15 @@ class DashboardPostController extends Controller
         }
 
         $validatedData = $request->validate($rules);
+
+        // Mengecek apakah ada image baru
+        if ($request->file('image')) {
+            // Untuk menghapus image lama
+            if ($request->oldImage) {
+                Storage::delete($request->oldImage);
+            }
+            $validatedData['image'] = $request->file('image')->store('post-images');
+        }
 
         $validatedData['user_id'] = auth()->user()->id;
         $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
@@ -118,6 +129,11 @@ class DashboardPostController extends Controller
     // untuk delete postingan
     public function destroy(Post $post)
     {
+        // Untuk menghapus image
+        if ($post->image) {
+            Storage::delete($post->image);
+        }
+
         Post::destroy($post->id);
         return redirect('/dashboard/posts')->with('success', 'Post has been deleted!');
     }
